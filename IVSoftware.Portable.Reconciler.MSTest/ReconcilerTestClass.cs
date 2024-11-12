25a649a62e7a8827bb8ce27824bbbb854ebbcf05
Tom Gregor <tgregor@ivsoftware.com>
2024-11-12 11:29:29 -0700

    ORIGIN: MSTest project

---
+++ IVSoftware.Portable.Reconciler.MSTest/ReconcilerTestClass.cs
@@ -0,0 +1,15 @@
+using IVSoftware.Portable.Disposable;
+using IVSoftware.WinOS.MSTest.Extensions;
+using Newtonsoft.Json;
+using SQLite;
+using System.Diagnostics;
+using static IVSoftware.Portable.Reconciler;
+using static IVSoftware.WinOS.MSTest.Extensions.Static;
+
+namespace IVSoftware.Portable.Static.Reconciler.MSTest
+{
+    [TestClass]
+    public class ReconcilerTestClass
+    {
+    }
+}
